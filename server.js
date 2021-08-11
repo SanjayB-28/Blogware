9a68a1f3764540340cc6944d6788749e2137af56
Sanjay <sanjaysmack@gmail.com>
2021-08-11 09:01:30 +0530

    commit

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article= require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
 mongoose.connect('mongodb://localhost/blogware',{ 
-   useNewUrlParser: true, useUnifiedTopology: true })
+   useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true})
 
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/',(req,res)=>{
-    const articles = [{
-       title: 'Test Article 1',
-       createdAt: new Date(),
-       description: 'Test description'
-    },
-    {
-       title: 'Test Article 2',
-       createdAt: new Date(),
-       description: 'Test description 2'
-    }]
+app.get('/', async (req,res)=>{
+    const articles = await Article.find().sort({
+       createdAt: 'desc'})
     res.render('articles/index', {articles: articles})
 })
 
